9c5c06ea3667cbdf039eb441f49c6f96d0958543
Simo Råman <simoraman@gmail.com>
2013-12-29 14:20:12 +0200

    play three hardcoded samples

--- src/com/example/biitbox/FullscreenActivity.java
+++ src/com/example/biitbox/FullscreenActivity.java
@@ -1,41 +1,38 @@
 package com.example.biitbox;
 
-import java.io.File;
-
 import android.app.Activity;
-import android.media.AudioManager;
-import android.media.SoundPool;
-import android.media.SoundPool.OnLoadCompleteListener;
 import android.os.Bundle;
-import android.os.Environment;
 import android.view.View;
 
 public class FullscreenActivity extends Activity {
-	boolean loaded = false;
-	SoundPool soundPool = null;
-	int sound = 0;
+	
+	private SoundService soundService;
+	
 	public void play1(View view){
-		if(loaded){
-			soundPool.play(sound, 1.0f, 1.0f, 0, 0, 1.0f);
+		switch (view.getId()) {
+			case R.id.button1:
+				play(1);
+				break;
+			case R.id.button2:
+				play(2);
+				break;
+			case R.id.button3:
+				play(3);
+				break;
+		}
+	}
+
+	private void play(Integer sound) {
+		if(soundService.isReady()){
+			soundService.playSample(sound);
 		}
 	}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        File root = new File(Environment.getExternalStorageDirectory(), "");
-		 File gpxfile = new File(root, "sound1.wav");
-		soundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
-		
-		soundPool.setOnLoadCompleteListener(new OnLoadCompleteListener() {
-           @Override
-           public void onLoadComplete(SoundPool soundPool, int sampleId,
-                   int status) {
-               loaded = true;
-           }
-       });
-		sound = soundPool.load(gpxfile.getPath(), 1);
+        soundService = new SoundService();
+        soundService.loadSounds();
 		       
         setContentView(R.layout.activity_fullscreen);
         
---
+++ src/com/example/biitbox/SoundService.java
@@ -0,0 +1,45 @@
+package com.example.biitbox;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.media.SoundPool.OnLoadCompleteListener;
+import android.os.Environment;
+
+public class SoundService {
+	SoundPool soundPool = null;
+	List<Integer> sounds = new ArrayList<Integer>();
+	List<Integer> ints = new ArrayList<Integer>();
+	public void loadSounds() {
+		soundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+		File root = new File(Environment.getExternalStorageDirectory(), "");
+		for(int i = 0; i < 3; i++){
+			int index = i+1;
+			File sound1 = new File(root, "sound" + index + ".wav");
+			String path = sound1.getPath();
+			int soundId = soundPool.load(path, 1);
+			sounds.add(soundId);
+		}
+		
+		
+		
+		soundPool.setOnLoadCompleteListener(new OnLoadCompleteListener() {
+           @Override
+           public void onLoadComplete(SoundPool soundPool, int sampleId,
+                   int status) {
+               ints.add(sampleId);
+           }
+       });
+		
+	}
+	public boolean isReady() {
+		return (ints.size() >= 3);
+	}
+	public void playSample(Integer sound) {
+		soundPool.play(sounds.get(sound-1), 1.0f, 1.0f, 0, 0, 1.0f);		
+	}
+
+}
